9f534b462b5beed470be87e6c0f4848c506a41df
janeduncan <janeduncan@live.co.uk>
2018-10-20 12:37:12 +0100

    Creates articles_categories join table

--- src/main/java/com/example/codeclan/NewsApplication/models/ArticleCategory.java
+++ src/main/java/com/example/codeclan/NewsApplication/models/ArticleCategory.java
@@ -1,16 +1,61 @@
 package com.example.codeclan.NewsApplication.models;
 
-public enum ArticleCategory {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name = "categories")
+public class ArticleCategory {
 
-    POLITICS,
-    SPORTS,
-    TECHNOLOGY,
-    BUSINESS,
-    LOCALNEWS,
-    WORLDNEWS,
-    UKNEWS,
-    ENVIRONMENT,
-    EDUCATION;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
 
+    @Column(name = "category")
+    private CategoryType category;
+
+    @JsonIgnoreProperties("categories")
+    @ManyToMany
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name = "articles_categories",
+            joinColumns = {@JoinColumn(name = "category_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="article_id", nullable = false, updatable = false)}
+    )
+    private List<Article> articles;
+
+    public ArticleCategory(CategoryType category) {
+        this.category = category;
+        this.articles = new ArrayList<>();
+    }
+
+    public ArticleCategory() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public CategoryType getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryType category) {
+        this.category = category;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 }
