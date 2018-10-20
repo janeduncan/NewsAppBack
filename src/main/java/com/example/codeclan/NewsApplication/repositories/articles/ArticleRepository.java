ee5c2def05ea584292e73be17e6388ba945ab8bb
Fiona Wilson <fionaw237@gmail.com>
2018-10-20 18:50:05 +0100

    Adds projection for article class

--- src/main/java/com/example/codeclan/NewsApplication/repositories/articles/ArticleRepository.java
+++ src/main/java/com/example/codeclan/NewsApplication/repositories/articles/ArticleRepository.java
@@ -1,7 +1,11 @@
 package com.example.codeclan.NewsApplication.repositories.articles;
 
 import com.example.codeclan.NewsApplication.models.Article;
+import com.example.codeclan.NewsApplication.projections.EmbedItems;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(excerptProjection = EmbedItems.class)
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
