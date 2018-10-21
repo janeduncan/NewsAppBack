646f41d738b4500c1d24879c4f0884e87a473a89
janeduncan <janeduncan@live.co.uk>
2018-10-21 23:28:41 +0100

    Adds new article titles to dataloader

--- src/main/java/com/example/codeclan/NewsApplication/components/DataLoader.java
+++ src/main/java/com/example/codeclan/NewsApplication/components/DataLoader.java
@@ -29,11 +29,47 @@ public class DataLoader implements ApplicationRunner {
 
     public void run(ApplicationArguments args) {
 
-        DateFormat df = new SimpleDateFormat("dd-MM-yy");
-        String newDate = "25-05-19";
-        Date date = null;
+        DateFormat df1 = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        String newDate1 = "19-10-2018 12:30";
+        Date date1 = null;
         try {
-            date = df.parse(newDate);
+            date1 = df1.parse(newDate1);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        DateFormat df2 = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        String newDate2 = "19-10-2018 07:45";
+        Date date2 = null;
+        try {
+            date2 = df2.parse(newDate2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        DateFormat df3 = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        String newDate3 = "19-10-2018 09:00";
+        Date date3 = null;
+        try {
+            date3 = df3.parse(newDate3);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        DateFormat df4 = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        String newDate4 = "19-10-2018 14:45";
+        Date date4 = null;
+        try {
+            date4 = df4.parse(newDate4);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        DateFormat df5 = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        String newDate5 = "19-10-2018 18:30";
+        Date date5 = null;
+        try {
+            date5 = df5.parse(newDate5);
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -68,33 +104,33 @@ public class DataLoader implements ApplicationRunner {
 
         // ARTICLES
 
-        Article article1 = new Article(date, Region.SCOTLAND, "Title", "Article", "Image URL", journalist1);
+        Article article1 = new Article(date1, Region.SCOTLAND, "Phasellus in molestie dolor, posuere pellentesque nisi.", "Article", "Image URL", journalist1);
         articleRepository.save(article1);
         article1.addCategory(politicsCategory);
         article1.setJournalist(journalist1);
         articleRepository.save(article1);
 
-        Article article2 = new Article(date, Region.LONDON, "Title", "Article", "Image URL", journalist1);
+        Article article2 = new Article(date2, Region.LONDON, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Article", "Image URL", journalist1);
         articleRepository.save(article2);
         article2.addCategory(ukNewsCategory);
         article2.setJournalist(journalist1);
         articleRepository.save(article2);
 
-        Article article3 = new Article(date, Region.SOUTHEAST, "Title", "Article", "Image URL", journalist1);
+        Article article3 = new Article(date3, Region.SOUTHEAST, "Phasellus placerat tellus ut rhoncus consequat.", "Article", "Image URL", journalist1);
         articleRepository.save(article3);
         article3.addCategory(politicsCategory);
         article3.addCategory(ukNewsCategory);
         article3.setJournalist(journalist1);
         articleRepository.save(article3);
 
-        Article article4 = new Article(date, Region.WALES, "Title", "Article", "Image URL", journalist2);
+        Article article4 = new Article(date4, Region.WALES, "Integer consectetur tellus sed ipsum hendrerit consectetur. ", "Article", "Image URL", journalist2);
         articleRepository.save(article4);
         article4.addCategory(techCategory);
         article4.addCategory(businessCategory);
         article4.setJournalist(journalist2);
         articleRepository.save(article4);
 
-        Article article5 = new Article(date, Region.WESTMIDLANDS, "Title", "Article", "Image URL", journalist2);
+        Article article5 = new Article(date5, Region.WESTMIDLANDS, "Ut fermentum nisi et placerat varius. Aliquam et.", "Article", "Image URL", journalist2);
         articleRepository.save(article5);
         article5.addCategory(techCategory);
         article5.addCategory(ukNewsCategory);
